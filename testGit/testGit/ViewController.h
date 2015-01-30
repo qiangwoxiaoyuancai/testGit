820bb93e98182d973b0e74a8ed4c77a2bb56e263
xiaoyuancai <1601351210@qq.com>
2015-01-30 12:35:53 +0800

    commit from root

---
+++ testGit/testGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by leYuan on 15-1-30.
+//  Copyright (c) 2015年 leYuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testGit/testGit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  testGit
+//
+//  Created by leYuan on 15-1-30.
+//  Copyright (c) 2015年 leYuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
